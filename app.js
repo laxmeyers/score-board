b49ba8c96b6b876d7b930c06574722355e7da327
Andrew Meyers <laxmeyers@gmail.com>
2023-01-23 15:12:08 -0700

    scoreboard

---
+++ app.js
@@ -0,0 +1,31 @@
+let score1 = document.getElementById('team-1-score')
+let score2 = document.getElementById('team-2-score')
+let team1 = 0
+let team2 = 0
+
+function reset(){
+    score1.innerText = 00
+    score2.innerText = 00
+    team1 = 0
+    team2 = 0
+}
+
+function addTeamOneOne(){
+    team1 += 1
+    score1.innerText = team1
+}
+
+function addTeamTwoOne(){
+    team2 += 1
+    score2.innerText = team2
+}
+
+function addTeamOneThree(){
+    team1 += 3
+    score1.innerText = team1
+}
+
+function addTeamTwoThree(){
+    team2 += 3
+    score2.innerText = team2
+}
